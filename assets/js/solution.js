340df3a9df15afe5d79db496afebb0a759206fe1
lapie002 <lapierre.bruno@gmail.com>
2017-03-15 09:50:44 +0100

    solution changed

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,32 +1,35 @@
-var boucler = true
+var boucler = true;
 
 var members = ['Aida67','lapie002','anneserrano','Jennysmille','nunkabuk','RCosson','kaonb-ax','FerEmilie','crazychouwi', 'KiluaZoldyc','patatobeur','Sam11360','elo062','hermeline','Biciclet'];
-document.getElementById("check").addEventListener("click",function(){
 
-  if(boucler){
-    for (var i = 0; i < members.length; i++) {
-      switch(members[i]) {
+document.getElementById("check").addEventListener("click",function()
+{
+
+  if(boucler)
+  {
+
+    for (var i = 0; i < members.length; i++)
+    {
+      switch(members[i])
+      {
           case "anneserrano":
               var newdiv = document.createElement("div");
-              var divcheck = document.getElementById("check");
+              var divcheck = document.getElementById("school_members");
 
-              newdiv.innerHTML = "<p> Affiche "+ members[i] + "</p>"
+              newdiv.innerHTML += "<p> Affiche "+ members[i] + "</p>"
               divcheck.appendChild(newdiv);
 
               console.log("affiche"+" "+ members[i]);
               break;
 
           default:
+              var newdiv = document.createElement("div");
+              var divcheck = document.getElementById("school_members");
 
-          var newdiv = document.createElement("div");
-          var divcheck = document.getElementById("check");
-
-          newdiv.innerHTML = "<p>" + members[i] + "</p>"
-          divcheck.appendChild(newdiv);
-
-
+              newdiv.innerHTML += "<p>" + members[i] + "</p>"
+              divcheck.appendChild(newdiv);
               console.log(members[i]);
-    }
+      }
     }
   }
 
